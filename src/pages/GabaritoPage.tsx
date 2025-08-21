1c568a256dd92b5cd63de991059dfad2248cba11
hendricksonweib <hendricksonrocha22@gmail.com>
2025-08-21 16:23:15 -0300

    feat: update API URL for camera capture functionality

--- src/pages/GabaritoPage.tsx
+++ src/pages/GabaritoPage.tsx
@@ -3,7 +3,7 @@ import { Header } from "../components/Header";
 import CameraCapture from "../layout/CameraCapture";
 
 export default function GabaritoPage() {
-  const apiUrl = "https://gerador-gabarito-corretor-experimental.lh6c5d.easypanel.host/api/leitor/";
+  const apiUrl = "https://d89002badc9f.ngrok-free.app/api/leitor/";
   return (
     <>
       <Header />
