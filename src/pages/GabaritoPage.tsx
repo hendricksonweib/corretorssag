0714ce369fd792dea9dd5b4b12a62c72a024ce8f
hendricksonweib <hendricksonrocha22@gmail.com>
2025-08-24 11:08:48 -0300

    Feat: novas coisas

--- src/pages/GabaritoPage.tsx
+++ src/pages/GabaritoPage.tsx
@@ -1,14 +1,16 @@
-
 import { Header } from "../components/Header";
 import CameraCapture from "../layout/CameraCapture";
 
 export default function GabaritoPage() {
   const apiUrl = "https://gerador-gabarito-corretor-experimental.lh6c5d.easypanel.host/api/leitor/";
+
   return (
     <>
       <Header />
-      <div className="pt-20 p-12 bg-gray-100 min-h-screen">
-        <CameraCapture apiUrl={apiUrl} />
+      <div className="pt-20 p-6 bg-gray-100 min-h-screen">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <CameraCapture apiUrl={apiUrl} />
+        </div>
       </div>
     </>
   );
