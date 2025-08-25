dd5ea29ac0d2a379265d78a79f0503cbae66ae4b
DaviCortesDev <davicortes1@hotmail.com>
2025-08-25 14:52:51 -0300

    Colocando rota corrtor api no env

--- src/pages/GabaritoPage.tsx
+++ src/pages/GabaritoPage.tsx
@@ -2,7 +2,7 @@ import { Header } from "../components/Header";
 import CameraCapture from "../layout/CameraCapture";
 
 export default function GabaritoPage() {
-  const apiUrl = "https://gerador-gabarito-corretor-experimental.lh6c5d.easypanel.host/api/leitor/";
+  const apiUrl = `${import.meta.env.VITE_API_CORRETOR}/api/leitor/`;
 
   return (
     <>
